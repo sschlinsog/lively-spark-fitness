fad90b18f72501ff45836c9ed47cd2d7efb2e0e9
sschlinsog <Samuel.Schlinsog@gmail.com>
2015-07-02 10:29:23 -0500

    started over with the design of the site with resonsive mobile first in mind

--- js/index.js
+++ js/index.js
@@ -1,13 +1,30 @@
 $(function() {
   
+  $('#fullscreenvid').click(function() {
+    openVideo();
+    updateVideo();
+  });
+  
   $('#intro-movie').on('ended', function() {
     $(this).fadeOut();
-    $('#overlay').fadeOut();
-  });
-
-  $('#overlay').click(function() {
-    $('#intro-movie').stop().fadeOut();
-    $(this).fadeOut();
+    $('#overlay-bg').fadeOut();
   });
   
 });
+
+
+function openVideo() {
+  $('#intro-movie').fadeIn();
+  $('#overlay-bg').fadeIn();
+  $('#intro-movie').get(0).play();
+}
+
+function updateVideo() {
+  var $videoWindow = $('#intro-movie');
+  var top = '50px';
+  var left = ($(window).width() - $videoWindow.outerWidth()) / 2;
+  $videoWindow.css({
+    "top" : top,
+    "left" : left
+  });
+}
